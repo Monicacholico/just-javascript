8b674df30a793590156539072d1bc8d0063a5a9e
Monicacholico <monicachol@hotmail>
2021-03-12 22:08:43 -0600

    saving numbers

--- playground/custom-functions/some.js
+++ playground/custom-functions/some.js
@@ -1,1 +1,3 @@
 console.log('im in another folder');
+
+
--- playground/custom-functions/types.js
+++ playground/custom-functions/types.js
@@ -9,3 +9,12 @@ const sentence2 = `she's so "cool"`;
 const song = 'oohh\
 my\
 good';
+
+
+const age = 100;
+// const name = 'wes';
+
+const smarties = 20;
+const kids = 3;
+const eachKidGets = Math.floor(smarties / kids);
+console.log(`Each kids gets ${eachKidGets}`);
