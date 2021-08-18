1197de000d59b8ffde9b5ddb08d9e3acae060bbb
Monicacholico <monicachol@hotmail.com>
2021-08-17 21:24:57 -0700

    chapter 12

--- playground/custom-functions/cs.js
+++ playground/custom-functions/cs.js
@@ -4,3 +4,25 @@ function calculateBill(billAmount, taxRate = 0.13, tipRate = 0.15) {
     const total = billAmount + billAmount * taxRate + billAmount * tipRate;
     return total;
   }
+
+  const firstName = 'wes';
+  function sayHiTo(firstName) {
+    return `Hello ${firstName}`;
+  }
+
+
+  const greeting = sayHiTo('wes')
+  console.log(greeting);
+
+  const myTotals13 = calculateBill(20 + 20 + 30 + 20, 0.15);
+  console.log(myTotals13);
+
+  function doctorize(name) {
+    return `Dr ${name}`;
+  }
+
+  function yell(name) {
+    return `HEY ${name.toUpperCase()}`;
+  }
+
+  console.log(yell(doctorize('wes')));
